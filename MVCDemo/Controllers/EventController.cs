0019dca11518b64ece0d34c491396b7951b76a48
ETG1 <ETG1@FNXWSF2-SHEL>
2022-09-08 18:19:36 +0530

    Done

---
+++ MVCDemo/Controllers/EventController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+
+
+
+namespace MVCDemo.Controllers
+{
+    public class EventController : Controller
+    {
+        private List<Booking> events;
+
+
+
+        public IActionResult Index()
+        {
+            events = new List<Booking>();
+            events.Add(new Booking { Title = "ask CEO", EventsDate = DateTime.Today, Qty = 10 });
+            return View(events);
+        }
+    }
+}
---
+++ MVCDemo/Models/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCDemo.Models
+{
+    public class Booking
+    {
+        public string Title { get; set; }
+        public DateTime EventsDate { get; set; }
+        public int Qty { get; set; }
+
+
+    }
+}
