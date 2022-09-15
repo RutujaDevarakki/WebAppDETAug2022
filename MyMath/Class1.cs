9420d18b861ac3d2dd5095921d0605d8c9c6be3c
ETG1 <ETG1@FNXWSF2-SHEL>
2022-09-15 16:51:40 +0530

    TDDDemo

---
+++ MyMath/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMath
+{
+    public class Class1
+    {
+
+    }
+
+    public class Rooter
+    {
+        public double SquareRoot(double input)
+        {
+            return input / 2;
+            //hrow new NotImplementedException();
+        }
+    }
+}
